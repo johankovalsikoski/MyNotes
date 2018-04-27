7849dcaf461b2c586173c5eebdf641c6d18507bc
Johan Kovalsikoski <johank@dbserver.com.br>
2018-04-27 10:12:58 -0300

    Added primary key anotation to Note id

--- app/src/main/java/com/kovalsikoski/johan/mynotes/Note.kt
+++ app/src/main/java/com/kovalsikoski/johan/mynotes/Note.kt
@@ -1,8 +1,10 @@
 package com.kovalsikoski.johan.mynotes
 
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 
 open class Note(
+        @PrimaryKey
         var id: Int = -1,
         var title: String = "",
         var description: String = "") : RealmObject()

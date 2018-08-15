e884e9155fe2517283a2ce2099e52e6977c6fec3
Johan <johank@dbserver.com.br>
2018-08-15 15:32:45 -0300

    Added uuid to user

--- app/src/main/java/com/kovalsikoski/johan/mynotes/UserModel.kt
+++ app/src/main/java/com/kovalsikoski/johan/mynotes/UserModel.kt
@@ -5,6 +5,7 @@ import io.realm.annotations.PrimaryKey
 
 open class UserModel (
         @PrimaryKey
+        var uuid: String = "",
         var email: String = "",
         var passwordTip: String = "",
         var password: String = "") : RealmObject()
